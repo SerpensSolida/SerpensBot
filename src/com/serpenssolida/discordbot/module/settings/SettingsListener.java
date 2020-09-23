fba3a81249e79bdb9505e0dc6b22183213e0fae0
Alex Salemi <alex_s96@hotmail.it>
2020-09-24 01:59:57 +0200

    Reworked message sent by prefix command.

--- src/com/serpenssolida/discordbot/module/settings/SettingsListener.java
+++ src/com/serpenssolida/discordbot/module/settings/SettingsListener.java
@@ -3,6 +3,7 @@ package com.serpenssolida.discordbot.module.settings;
 import com.serpenssolida.discordbot.module.BotListener;
 import com.serpenssolida.discordbot.BotMain;
 import com.serpenssolida.discordbot.module.Command;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
@@ -86,12 +87,17 @@ public class SettingsListener extends BotListener
 		String moduleID; //Module id passed to the command.
 		String modulePrefix; //Module prefix passed to the command.
 		Member authorMember = guild.retrieveMember(author).complete(); //Member that sent the command.
-		MessageBuilder builder = new MessageBuilder();
+		MessageBuilder messageBuilder = new MessageBuilder();
+		EmbedBuilder embedBuilder = new EmbedBuilder();
+		
+		//Add footer
+		embedBuilder.setFooter("Richiesto da " + author.getName(), author.getAvatarUrl());
+		embedBuilder.setAuthor(BotMain.api.getSelfUser().getName(), "https://github.com/SerpensSolida/SerpensBot", BotMain.api.getSelfUser().getAvatarUrl());
 		
 		if (args == null)
 		{
 			//Send the list of modules and their prefixes.
-			builder.append("> Lista dei moduli e dei loro prefissi: \n> \n");
+			embedBuilder.setTitle("Lista dei moduli e dei loro prefissi");
 			
 			for (Object registeredListener : BotMain.api.getEventManager().getRegisteredListeners())
 			{
@@ -99,44 +105,31 @@ public class SettingsListener extends BotListener
 				{
 					BotListener listener = (BotListener) registeredListener;
 					
-					builder.appendFormat("> ID: `%s` -> Prefisso: `%s`\n", listener.getInternalID(), listener.getModulePrefix());
+					embedBuilder.addField("Modulo " + listener.getModuleName(), String.format("ID modulo: `%s`\nPrefisso: `%s`", listener.getInternalID(), listener.getModulePrefix()), true);
 				}
 			}
+			
+			messageBuilder.setEmbed(embedBuilder.build());
 		}
 		else if (args.length == 1)
 		{
 			moduleID = args[0];
-			BotListener listener = null;
-			
-			//Get the module with the correct id and send its module prefix.
-			for (Object registeredListener : BotMain.api.getEventManager().getRegisteredListeners())
-			{
-				if (registeredListener instanceof BotListener)
-				{
-					BotListener l = (BotListener) registeredListener;
-					
-					if (l.getInternalID().equals(moduleID))
-					{
-						listener = l;
-						break;
-					}
-				}
-			}
+			BotListener listener = this.getListenerById(moduleID);
 			
 			if (listener != null)
 			{
-				builder.appendFormat("> Il prefisso del modulo `%s` è `%s`.", listener.getInternalID(), listener.getModulePrefix());
+				messageBuilder.appendFormat("> Il prefisso del modulo `%s` è `%s`.", listener.getInternalID(), listener.getModulePrefix());
 			}
 			else
 			{
-				builder.appendFormat("> Modulo con id `%s` non trovato.", moduleID);
+				messageBuilder.appendFormat("> Modulo con id `%s` non trovato.", moduleID);
 			}
 		}
 		else
 		{
 			moduleID = args[0];
 			modulePrefix = args[1];
-			BotListener listener = null;
+			BotListener listener = this.getListenerById(moduleID);
 			
 			if (!this.isAdmin(authorMember) && !authorMember.isOwner())
 			{
@@ -144,35 +137,39 @@ public class SettingsListener extends BotListener
 				return;
 			}
 			
-			//Get the module with the correct id and sets its module prefix.
-			for (Object registeredListener : BotMain.api.getEventManager().getRegisteredListeners())
-			{
-				if (registeredListener instanceof BotListener)
-				{
-					BotListener l = (BotListener) registeredListener;
-					
-					if (l.getInternalID().equals(moduleID))
-					{
-						listener = l;
-						break;
-					}
-				}
-			}
-			
 			if (listener != null)
 			{
 				listener.setModulePrefix(modulePrefix);
 				BotMain.saveSettings();
 				
-				builder.appendFormat("> Prefisso del modulo `%s` è stato impostato a `%s`", listener.getInternalID(), listener.getModulePrefix());
+				messageBuilder.appendFormat("> Prefisso del modulo `%s` è stato impostato a `%s`", listener.getInternalID(), listener.getModulePrefix());
 			}
 			else
 			{
-				builder.appendFormat("> Modulo con id `%s` non trovato.", moduleID);
+				messageBuilder.appendFormat("> Modulo con id `%s` non trovato.", moduleID);
 			}
 		}
 		
-		channel.sendMessage(builder.build()).queue();
+		channel.sendMessage(messageBuilder.build()).queue();
+	}
+	
+	private BotListener getListenerById(String moduleID)
+	{
+		//Get the module with the correct id and send its module prefix.
+		for (Object registeredListener : BotMain.api.getEventManager().getRegisteredListeners())
+		{
+			if (registeredListener instanceof BotListener)
+			{
+				BotListener listener = (BotListener) registeredListener;
+				
+				if (listener.getInternalID().equals(moduleID))
+				{
+					return listener;
+				}
+			}
+		}
+		
+		return null;
 	}
 	
 	private void setBotCommandSymbol(Guild guild, MessageChannel channel, User author, String[] args)
