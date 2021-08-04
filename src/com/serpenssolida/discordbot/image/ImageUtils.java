e988c012ee188fd7da1adf6531df767cb0650eb0
Alex Salemi <alex_s96@hotmail.it>
2021-08-04 18:56:57 +0200

    Added utility class ImageUtils. Added method for converting a BufferedImage to a byte array.

---
+++ src/com/serpenssolida/discordbot/image/ImageUtils.java
@@ -0,0 +1,27 @@
+package com.serpenssolida.discordbot.image;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class ImageUtils
+{
+	public static byte[] toByteArray(BufferedImage image)
+	{
+		ByteArrayOutputStream outputStream;
+		
+		try
+		{
+			outputStream = new ByteArrayOutputStream();
+			ImageIO.write(image, "png", outputStream);
+		}
+		catch (IOException e)
+		{
+			outputStream = null;
+			e.printStackTrace();
+		}
+		
+		return outputStream.toByteArray();
+	}
+}
